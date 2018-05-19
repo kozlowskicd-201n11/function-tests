ec0c80abb97c55cc7eba9ccd5fa8ccc373b469da
Chris Kozlowski <kozlowskicd@gmail.com>
2018-05-19 11:29:07 -0700

    Problem 3 Solved

--- app.js
+++ app.js
@@ -42,12 +42,17 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+function sumAndMultiply(a, b, c) {
+    var subSum = parseInt(sum(a, b));
+    var totalSum = parseInt(sum(subSum, c));
+    var subMult = parseInt(multiply(a, b));
+    var totalMult = parseInt(multiply(subMult, c));
+    var finalTotal = [totalSum, totalMult, (a + ' and ' + b + ' and ' + c + ' sum to ' + totalSum + '.'), ('The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + totalMult + '.')];
+    return finalTotal;
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
